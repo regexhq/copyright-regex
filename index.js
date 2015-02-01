ad4f48dc2628a701e61991259a931bfd231ca3d7
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-01 03:21:09 -0500

    clean up

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /*!
- * copyright-regex <https://github.com/jonschlinkert/copyright-regex>
+ * copyright-regex <https://github.com/regexps/copyright-regex>
  *
- * Copyright (c) 2015 Jon Schlinkert, contributors.
+ * Copyright (c) 2015 Jon Schlinkert.
  * Licensed under the MIT license.
  */
 
 'use strict';
 
 module.exports = function copyrightRegex() {
-  return /(?!.*(?:\{|\}|\);))(?:(copyright)[ \t]*(?:(&copy;|\(c\)|&#(?:169|xa9;)|©)[ \t]+)?)(?:(?:((?:19|20)[0-9]+)[^\w]*)*([\w\s]*)*)/i;
+  return /(?!.*(?:\{|\}|\);))(?:(copyright)[ \t]*(?:(&copy;|\(c\)|&#(?:169|xa9;)|©)[ \t]+)?)(?:((?:((?:(?:19|20)[0-9]{2}))[^\w]*)*)([\w\s]*))/i;
 };
